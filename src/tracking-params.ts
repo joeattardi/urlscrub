dd4dec8742ffe5662fb808f9940c59c45c282b14
Joe Attardi <jattardi@gmail.com>
2025-03-22 16:00:15 -0400

    Add tracking parameter warning

---
+++ src/tracking-params.ts
@@ -0,0 +1,18 @@
+export const trackingParameters = [
+    'utm_source',
+    'utm_medium',
+    'utm_campaign',
+    'utm_term',
+    'utm_content',
+    'fbclid',
+    'gclid',
+    'msclkid',
+    'ref',
+    '_ga',
+    'mc_cid',
+    'mc_eid',
+    'yclid',
+    'dclid',
+    'zanpid',
+    'igshid'
+];
--- src/url-builder/QueryParameters.tsx
+++ src/url-builder/QueryParameters.tsx
@@ -1,19 +1,27 @@
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { PiPlus, PiXCircle } from 'react-icons/pi';
-import { QueryParameter } from './types';
 import { Alert, AlertDescription } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
+import { Input } from '@/components/ui/input';
+import clsx from 'clsx';
+import { PiPlus, PiWarningDuotone, PiXCircle } from 'react-icons/pi';
+import { trackingParameters } from '../tracking-params';
+import { QueryParameter } from './types';
 
 type QueryParametersProps = {
     parameters: QueryParameter[];
     onDelete: (id: string) => void;
     onAdd: () => void;
     onUpdate: (parameter: QueryParameter) => void;
-}
+};
 
-export default function QueryParameters({ parameters, onDelete, onAdd, onUpdate }: QueryParametersProps) {
+export default function QueryParameters({
+    parameters,
+    onDelete,
+    onAdd,
+    onUpdate
+}: QueryParametersProps) {
     function onChange(event: React.ChangeEvent<HTMLInputElement>, id: string) {
-        const parameter = parameters.find(p => p.id === id);
+        const parameter = parameters.find((p) => p.id === id);
         if (parameter) {
             onUpdate({
                 ...parameter,
@@ -40,25 +48,56 @@ export default function QueryParameters({ parameters, onDelete, onAdd, onUpdate 
             </div>
             {parameters.length === 0 && (
                 <Alert className="mt-4 bg-zinc-50">
-                    <AlertDescription className="justify-items-center p-2">No query parameters added yet.</AlertDescription>
+                    <AlertDescription className="justify-items-center p-2">
+                        No query parameters added yet.
+                    </AlertDescription>
                 </Alert>
             )}
             <div className="flex flex-col gap-4 mt-4">
                 {parameters.map((parameter) => (
-                    <div key={parameter.id} className="grid md:grid-cols-[1fr_1fr_auto] gap-4 bg-slate-50 border border-slate-200 rounded p-4">
+                    <div
+                        key={parameter.id}
+                        className={clsx(
+                            'grid md:grid-cols-[24px_1fr_1fr_auto] gap-4 items-center bg-slate-50 border border-slate-200 rounded p-4',
+                            {
+                                'bg-yellow-50 border-yellow-200': trackingParameters.includes(
+                                    parameter.name
+                                )
+                            }
+                        )}
+                    >
+                        {trackingParameters.includes(parameter.name) ? (
+                            <HoverCard>
+                                <HoverCardTrigger>
+                                    <div className="flex items-center gap-2">
+                                        <PiWarningDuotone className="text-yellow-500 text-2xl" />
+                                        <div className="font-bold md:hidden">Tracking Parameter</div>
+                                    </div>
+                                </HoverCardTrigger>
+                                <HoverCardContent>
+                                    <h4 className="font-bold">Privacy Warning</h4>
+                                    <div className="text-sm">
+                                        <strong>{parameter.name}</strong> is a parameter used for
+                                        analytics and user tracking.
+                                    </div>
+                                </HoverCardContent>
+                            </HoverCard>
+                        ) : (
+                            <div />
+                        )}
                         <Input
                             className="bg-white"
                             placeholder="Name"
                             name="name"
                             value={parameter.name}
-                            onChange={event => onChange(event, parameter.id)}
+                            onChange={(event) => onChange(event, parameter.id)}
                         />
                         <Input
                             className="bg-white"
                             placeholder="Value"
                             value={parameter.value}
                             name="value"
-                            onChange={event => onChange(event, parameter.id)}
+                            onChange={(event) => onChange(event, parameter.id)}
                         />
                         <Button
                             variant="ghost"
